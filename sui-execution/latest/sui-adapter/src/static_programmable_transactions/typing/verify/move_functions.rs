b6109cbc096201abf4e874093327692c018240b0
Todd Nowacki <tmn@mystenlabs.com>
2025-08-27 15:00:01 -0700

    rewrite move_function taint

--- sui-execution/latest/sui-adapter/src/static_programmable_transactions/typing/verify/move_functions.rs
+++ sui-execution/latest/sui-adapter/src/static_programmable_transactions/typing/verify/move_functions.rs
@@ -51,7 +51,7 @@ impl Context {
 
     /// Marks mutable usages as hot. We don't care about `Move` since the value will be moved
     /// and that location is no longer accessible.
-    fn mark_hot(&mut self, arg: &T::Argument) {
+    fn mark_mut_hot(&mut self, arg: &T::Argument) {
         match &arg.value.0 {
             T::Argument__::Borrow(/* mut */ true, loc) => self.mark_loc_hot(*loc),
             T::Argument__::Borrow(/* mut */ false, _)
@@ -61,6 +61,7 @@ impl Context {
         }
     }
 
+    /// Helper for mark_mut_hot, should not be called directly
     fn mark_loc_hot(&mut self, location: T::Location) {
         match location {
             T::Location::TxContext => (), // TxContext is never hot, so nothing to do
@@ -103,94 +104,78 @@ fn command<Mode: ExecutionMode>(
     context: &mut Context,
     sp!(_, c): &T::Command,
 ) -> Result<Vec<IsHot>, ExecutionError> {
-    let result = &c.result_type;
-    Ok(match &c.command {
-        T::Command__::MoveCall(call) => move_call::<Mode>(env, context, call, result)?,
-        T::Command__::TransferObjects(objs, recipient) => {
-            arguments(env, context, objs);
-            argument(env, context, recipient);
-            vec![]
+    let T::Command_ {
+        command,
+        result_type,
+        drop_values: _,
+        consumed_shared_objects,
+    } = c;
+    let consumes_shared_objects = !consumed_shared_objects.is_empty();
+    let hot_arg = arguments(env, context, command.arguments());
+    let result_is_hot = match command {
+        T::Command__::MoveCall(call) => move_call::<Mode>(env, context, call, hot_arg)?,
+        T::Command__::TransferObjects(_, _)
+        | T::Command__::SplitCoins(_, _, _)
+        | T::Command__::MergeCoins(_, _, _)
+        | T::Command__::MakeMoveVec(_, _)
+        | T::Command__::Publish(_, _, _)
+        | T::Command__::Upgrade(_, _, _, _, _) => false,
+    };
+    let is_hot = consumes_shared_objects || hot_arg.is_some() || result_is_hot;
+    if is_hot {
+        for arg in command.arguments() {
+            context.mark_mut_hot(arg);
         }
-        T::Command__::SplitCoins(_, coin, amounts) => {
-            let amounts_are_hot = arguments(env, context, amounts);
-            let coin_is_hot = argument(env, context, coin);
-            let is_hot = amounts_are_hot || coin_is_hot;
-            if is_hot {
-                context.mark_hot(coin);
-            }
-            vec![is_hot; result.len()]
-        }
-        T::Command__::MergeCoins(_, target, coins) => {
-            let is_hot = arguments(env, context, coins);
-            argument(env, context, target);
-            if is_hot {
-                context.mark_hot(target);
-            }
-            vec![]
-        }
-        T::Command__::MakeMoveVec(_, args) => {
-            let is_hot = arguments(env, context, args);
-            debug_assert_eq!(result.len(), 1);
-            vec![is_hot]
-        }
-        T::Command__::Publish(_, _, _) => {
-            debug_assert_eq!(Mode::packages_are_predefined(), result.is_empty());
-            debug_assert_eq!(!Mode::packages_are_predefined(), result.len() == 1);
-            result.iter().map(|_| false).collect::<Vec<_>>()
-        }
-        T::Command__::Upgrade(_, _, _, ticket, _) => {
-            debug_assert_eq!(result.len(), 1);
-            let result = vec![false];
-            argument(env, context, ticket);
-            result
-        }
-    })
+    }
+    Ok(vec![is_hot; result_type.len()])
 }
 
-fn arguments(env: &Env, context: &Context, args: &[T::Argument]) -> bool {
-    args.iter().any(|arg| argument(env, context, arg))
+/// Returns the index of the first hot argument, if any
+fn arguments<'a>(
+    env: &Env,
+    context: &Context,
+    args: impl IntoIterator<Item = &'a T::Argument>,
+) -> Option<usize> {
+    args.into_iter()
+        .enumerate()
+        .find(|(_idx, arg)| argument(env, context, arg))
+        .map(|(idx, _arg)| idx)
 }
 
 fn argument(_env: &Env, context: &Context, arg: &T::Argument) -> bool {
     context.is_hot(arg)
 }
 
+/// Checks a move call for
+/// - valid signature (no references in return type)
+/// - valid visibility
+/// - private generics rules
+/// - if entry, no hot arguments
+/// Returns true iff any return type is a hot potato
 fn move_call<Mode: ExecutionMode>(
     env: &Env,
-    context: &mut Context,
+    _context: &mut Context,
     call: &T::MoveCall,
-    result: &T::ResultType,
-) -> Result<Vec<IsHot>, ExecutionError> {
+    hot_arg: Option<usize>,
+) -> Result<IsHot, ExecutionError> {
     let T::MoveCall {
         function,
-        arguments: args,
+        arguments: _,
     } = call;
     check_signature::<Mode>(function)?;
     check_private_generics(&function.runtime_id, function.name.as_ident_str())?;
     let (vis, is_entry) = check_visibility::<Mode>(env, function)?;
     // check rules around hot arguments and entry functions
     let returns_hot_potato = has_hot_potato_return_type::<Mode>(function);
-    let args_hot = args
-        .iter()
-        .map(|arg| argument(env, context, arg))
-        .collect::<Vec<_>>();
-    let is_hot = returns_hot_potato || args_hot.iter().any(|&is_hot| is_hot);
-    if is_entry && matches!(vis, Visibility::Private) {
-        for (idx, &arg_is_hot) in args_hot.iter().enumerate() {
-            if arg_is_hot && !Mode::allow_arbitrary_values() {
-                return Err(command_argument_error(
-                    CommandArgumentError::InvalidArgumentToPrivateEntryFunction,
-                    idx,
-                ));
-            }
+    if is_entry && matches!(vis, Visibility::Private) && !Mode::allow_arbitrary_values() {
+        if let Some(hot_arg_idx) = hot_arg {
+            return Err(command_argument_error(
+                CommandArgumentError::InvalidArgumentToPrivateEntryFunction,
+                hot_arg_idx,
+            ));
         }
     }
-    if is_hot {
-        for arg in args {
-            context.mark_hot(arg);
-        }
-    }
-    Ok(vec![is_hot; result.len()])
+    Ok(returns_hot_potato)
 }
 
 fn check_signature<Mode: ExecutionMode>(
